2182bc5607ceade7e95f894e4ffe41be5e7d0fc0
mrshahriar <toshahriarshakil@gmail.com>
2023-03-23 15:13:48 +0600

    product image added

--- src/components/products/ProductImagesGallery.jsx
+++ src/components/products/ProductImagesGallery.jsx
@@ -16,13 +16,13 @@ export default function ProductImagesGallery({ product }) {
               'rounded-lg aspect-w-2 aspect-h-1 sm:aspect-w-2 sm:aspect-h-2'
             )}
           >
-            {/* <ContentfulImage
+            <ContentfulImage
               className='object-contain'
               alt={`Cover Image for ${image.fields.title}`}
               src={image.fields.file.url}
               width={image.fields.file.details.image.width}
               height={image.fields.file.details.image.height}
-            /> */}
+            />
           </div>
         )
       })}
